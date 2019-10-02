2a75db3d3829f906f1cad490a6c2fd82f4e0c733
yanglk <1085960411@qq.com>
2019-10-02 16:46:15 +0800

    Reverse_stack

--- src/com/yanglk/algorithm/static_/Main_test.java
+++ src/com/yanglk/algorithm/static_/Main_test.java
@@ -1,19 +1,19 @@
 package com.yanglk.algorithm.static_;
 
+import java.util.Stack;
+
 public class Main_test {
     public static void main(String[] args) {
 
-        Two_StacksQueue tsq=new Two_StacksQueue();
-        tsq.add(1);
-        tsq.add(2);
-        tsq.poll();
-        tsq.add(3);
-        tsq.add(4);
-        tsq.peek();
-        tsq.poll();
-        tsq.poll();
-        tsq.poll();
-        tsq.peek();
+        Stack<Integer> tsq=new Stack<>();
+        Reverse_stack s=new Reverse_stack();
+        tsq.push(1);
+        tsq.push(2);
+        tsq.push(3);
+        tsq.push(4);
+
+        s.reverse_Stack(tsq);
+
 
     }
 }
