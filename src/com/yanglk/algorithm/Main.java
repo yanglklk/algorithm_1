ea9942ab441ddf95c4c02e5fe3c5d64391edf900
yanglklk <1085960411@qq.com>
2020-06-12 15:11:14 +0800

    delete two

--- src/com/yanglk/algorithm/Main.java
+++ src/com/yanglk/algorithm/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("add git");
-        System.out.println("set git");
-        System.out.println("get git");
+
     }
 }
