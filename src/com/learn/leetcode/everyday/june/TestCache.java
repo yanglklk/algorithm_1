8deac4ee5eaf503c54f774f3061b22c87f732537
yangliankun <yangliankun@58.com>
2021-06-15 10:22:41 +0800

    updata .gitignore

--- src/com/learn/leetcode/everyday/june/TestCache.java
+++ src/com/learn/leetcode/everyday/june/TestCache.java
@@ -2,6 +2,6 @@ package com.learn.leetcode.everyday.june;
 
 public class TestCache {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello  ");
     }
 }
